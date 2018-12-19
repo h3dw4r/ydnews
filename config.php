4aa6cb5c223044b7f048769a7d8de6bb0d32bd64
h3dw4r <38338029+h3dw4r@users.noreply.github.com>
2018-12-19 14:53:22 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "865643032993991",
+"sign" => "3828794ba17e74020ed444100ee471695128ca35t",
+"token" => "dd3ec82eaa392c6b098f1bfa9adb57cd",
+"uuid" => "402656b6-baad-48e2-b605-ec6a51b1fc7c"
+];
